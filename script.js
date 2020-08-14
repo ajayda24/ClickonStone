1cbea1245948d8d9d066ace0e50519f7d06e641f
Ajay Daniel Trevor <59312969+ajayda24@users.noreply.github.com>
2020-08-14 18:38:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,15 @@
 document.getElementById("scoreForm").style.display= "none";
 var score = 0;
 var x;
-var randomButton = Math.floor(Math.random()*1)+50;
-var randomGreenButton = Math.floor(Math.random()*1)+79;
-var whiteSpeed = 700;
-var greenSpeed = 600;
+var whiteSpeed = 600;
+var greenSpeed = 530;
 var intervalLoop;
 var intervalLoop2;
 var div = document.createElement("div");
 document.body.appendChild(div);
 div.setAttribute("class","divClass");
 
-if(score > 100){
-    whiteSpeed = 150;
-    greenSpeed = 125;
-} else if(score > 200){
-    whiteSpeed = 125;
-    greenSpeed = 100;
-} else if(score > 300){
-    whiteSpeed = 100;
-    greenSpeed = 75;
-} else if(score > 400){
-    whiteSpeed = 50;
-    greenSpeed = 35;
-}
+
 
 function startButton(){
 
@@ -82,7 +68,40 @@ function buttonGreen(){
         score = score+1;
         document.getElementById("scoreDisplay").innerHTML = "Score : " + score;
         winSound(this);
-        
+        if(score < 10){
+            whiteSpeed = 550;
+            greenSpeed = 500;
+        } else if(score > 20){
+            whiteSpeed = 500;
+            greenSpeed = 450;
+        } else if(score > 30){
+            whiteSpeed = 450;
+            greenSpeed = 400;
+        } else if(score > 50){
+            whiteSpeed = 400;
+            greenSpeed = 350;
+        } else if(score > 70){
+            whiteSpeed = 350;
+            greenSpeed = 300;
+        } else if(score > 90){
+            whiteSpeed = 300;
+            greenSpeed = 250;
+        } else if(score > 110){
+            whiteSpeed = 250;
+            greenSpeed = 200;
+        } else if(score > 130){
+            whiteSpeed = 200;
+            greenSpeed = 150;
+        } else if(score > 150){
+            whiteSpeed = 150;
+            greenSpeed = 100;
+        } else if(score > 170){
+            whiteSpeed = 100;
+            greenSpeed = 50;
+        } else if(score > 200){
+            whiteSpeed = 50;
+            greenSpeed = 25;
+        }
 
     });
     window.addEventListener('scroll', function() {
